4bfd57b9ae180db1f9b253218d9faae4ed0ca94b
Casey Morford <cmorford@gmail.com>
2017-06-12 11:36:29 -0500

    Sub app name underscores

--- newrelic/recipes/nodejs.rb
+++ newrelic/recipes/nodejs.rb
@@ -3,10 +3,6 @@ node[:deploy].each do |application, deploy|
     Chef::Log.debug("Skipping deploy::nodejs application #{application} as it is not a node.js app")
     next
   end
-  
-  Chef::Log.debug("Deploying newrelic.js")
-  
-  Chef::Log.debug("#{deploy}")
 
   template "#{deploy[:deploy_to]}/shared/config/newrelic.js" do
     cookbook 'newrelic'
@@ -15,7 +11,7 @@ node[:deploy].each do |application, deploy|
     owner deploy[:user]
     group deploy[:group]
     variables(
-      :application => deploy[:application],
+      :application => deploy[:application].gsub! '_', '-',
       :license_key => node[:newrelic][:license],
       :environment => node[:newrelic][:environment],
       :application_type => deploy[:application_type]
