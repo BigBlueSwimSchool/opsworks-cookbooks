f097fba3da1d6270117d65dd6a48d8582340042f
Casey Morford <cmorford@gmail.com>
2017-06-12 10:53:44 -0500

    newrelic.js config

---
+++ newrelic/recipes/nodejs.rb
@@ -0,0 +1,19 @@
+node[:deploy].each do |application, deploy|
+  if deploy[:application_type] != 'nodejs'
+    Chef::Log.debug("Skipping deploy::nodejs application #{application} as it is not a node.js app")
+    next
+  end
+
+  template "#{deploy[:deploy_to]}/newrelic.js" do
+    cookbook 'newrelic'
+    source 'newrelic.js.erb'
+    mode '0660'
+    owner deploy[:user]
+    group deploy[:group]
+    variables(
+      :license_key => node[:newrelic][:license],
+      :environment => node[:newrelic][:environment],
+      :application_type => deploy[:application_type]
+    )
+  end
+end
