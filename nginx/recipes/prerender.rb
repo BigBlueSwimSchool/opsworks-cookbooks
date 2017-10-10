a34e61d11ab5ff84c608b5623fc19bce7cb052b3
Casey Morford <cmorford@gmail.com>
2017-10-10 17:47:33 -0500

    Add pretender config

---
+++ nginx/recipes/prerender.rb
@@ -0,0 +1,21 @@
+
+template "#{node[:nginx][:dir]}/conf.d/prerender.io.conf" do
+  source "prerender.io.conf.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  variables(
+    :server_name => node[:server_name],
+    :client_protocol => node[:client_protocol],
+    :prerender => node[:prerender],
+    :proxy => node[:proxy]
+  )
+end
+
+include_recipe "nginx::service"
+
+Chef::Log.debug("nginx: restarting nginx")
+
+service "nginx" do
+  action :restart
+end
