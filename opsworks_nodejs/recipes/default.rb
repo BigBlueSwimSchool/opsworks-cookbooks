80341f41229d342891551bb8057e3bf6e5065e86
Casey Morford <casey@bigblueswim.school>
2018-06-25 23:46:54 -0500

    test

--- opsworks_nodejs/recipes/default.rb
+++ opsworks_nodejs/recipes/default.rb
@@ -38,9 +38,13 @@ else
     command "npm install -g forever"
   end
 
-  execute "install NPM_TOKEN" do
-    Chef::Log.debug("nodejs:install NPM_TOKEN")
-    command "set NPM_TOKEN #{node[:deploy][:environment_variables][:npm_token]}"
+  node[:deploy].each do |application, deploy|
+
+    execute "install NPM_TOKEN" do
+      Chef::Log.debug("nodejs:install NPM_TOKEN")
+      command "set NPM_TOKEN #{deploy[application][:environment_variables][:npm_token]}"
+    end
+
   end
   
 end
